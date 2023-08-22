f848292485e94af3902fabb49b5f2969a5faf2d0
Ernest Akhmirov <ernestakhmirov@gmail.com>
2023-08-22 22:34:00 +0300

    feed in progress

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -4,6 +4,7 @@ import OrderCard from "../components/OrderCard/OrderCard";
 import {useAppDispatch, useAppSelector} from "../utils/hooks";
 import {WS_CONNECTION_START, WS_CONNECTION_CLOSED} from "../services/constants";
 import {TWSOrder} from "../services/actions/WSActions";
+import constructorStyle from "../components/BurgerConstructor/BurgerConstructor.module.css";
 
 function FeedPage() {
 
@@ -20,15 +21,51 @@ function FeedPage() {
     }, [dispatch]);
 
     return (
-        <main className={styles.main}>
-            <h2 className={"text text_type_main-large pt-10 pb-5"}>Лента Заказов</h2>
-            <div>
-                {orders.map((order:TWSOrder) => (
-                    <OrderCard
-                        key={order._id}
-                        order={order}/>
-                ))}
+        <main className={styles.wrapper}>
 
+            <h2 className={"text text_type_main-large pt-10 pb-5"}>Лента Заказов</h2>
+            <div className={styles.main}>
+                <section className={styles.OrdersSection}>
+                    {orders.map((order: TWSOrder) => (
+                        <OrderCard
+                            key={order._id}
+                            order={order}/>
+                    ))}
+                </section>
+                <section className={styles.OrdersTable}>
+                    <div className={styles.OrdersStatus}>
+                        <div className={styles.Done}>
+                            <h2 className={"text text_type_main-medium mb-6"}>Готовы:</h2>
+                            <ul className={styles.OrdersList}>
+                                {orders.slice(0, 20).map((num: TWSOrder) => (
+                                    num.status === "done" ? (
+                                        <li key={num._id}
+                                            className={"text text_type_digits-default  text_color_success"}>
+                                            {num.number}
+                                        </li>
+                                    ) : null
+                                ))}
+                            </ul>
+                        </div>
+                        <div className={styles.Done}>
+                            <h2 className={"text text_type_main-medium mb-6"}>В работе:</h2>
+                            <ul className={styles.OrdersList}>
+                                {orders.slice(0, 20).map((num: TWSOrder) => (
+                                    num.status === "pending" ? (
+                                        <li key={num._id}
+                                            className={"text text_type_digits-default "}>
+                                            {num.number}
+                                        </li>
+                                    ) : null
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <p className={"text text_type_main-medium mt-15"}>Выполнено за все время:</p>
+                    <p className={`${styles.glow} text text_type_digits-large mt-6`}>123142</p>
+                    <p className={"text text_type_main-medium mt-15"}>Выполнено за все время:</p>
+                    <p className={`${styles.glow} text text_type_digits-large mt-6`}>122</p>
+                </section>
             </div>
         </main>
     );
